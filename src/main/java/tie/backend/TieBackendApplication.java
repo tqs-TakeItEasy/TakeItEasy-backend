a1ff3cee91043a0b04956f64b69e212cad06a17c
Tiago Sora <tiagogcarvalho2002@gmail.com>
2023-05-09 22:39:19 +0100

    spring boot set up

---
+++ src/main/java/tie/backend/TieBackendApplication.java
@@ -0,0 +1,13 @@
+package tie.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TieBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TieBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tie/backend/TieBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tie.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TieBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

5300b566a09942ae920c65c1c57bf5814be2ebe5
Tiago Sora <tiagogcarvalho2002@gmail.com>
2023-05-16 13:36:39 +0100

    trying ci for the first time

--- backend/src/main/java/tie/backend/model/Notification.java
+++ backend/src/main/java/tie/backend/model/Notification.java
@@ -1,9 +1,27 @@
 package tie.backend.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "notification")
 public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "company_email")
     private String companyEmail;
+
+    @Column(name = "user_email")
     private String userEmail;
+
+    @Column(name = "message")
     private String message;
 
     public Notification(){}
